c33988503ce46ebd2182e29f448fffa3a3436ca2
Pawel <pawel.nowak980901@gmail.com>
2024-02-15 12:48:21 +0100

    Push

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -4,4 +4,6 @@ from .models import Product
 urlpatterns = [
     path('', views.index,name='index'),
     path('product/<int:id>',views.detail,name='detail'),
+    path('success/',views.payment_success_view,name='success'),
+    path('failed/',views.payment_failed_view,name='failed'),
 ]
